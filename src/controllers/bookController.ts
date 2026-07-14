0215d55c58c20b498cec55284846d86d90edb313
DimRad26 <DimitrieOctavian.Radu@softwire.com>
2026-07-14 18:10:26 +0300

    Database query WORKS!

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { connection } from '../DB_Connection';
 
 class BookController {
     router: Router;
@@ -21,7 +22,39 @@ class BookController {
 
     getBooks(req: Request, res: Response) {
         try{
-            return res.json({})
+            var Request = require('tedious').Request;
+            const QUERY = 'SELECT * FROM Books;';
+            var books: any[] = []
+
+            const request = new Request(QUERY, (err, rowCount) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    return res.status(200).json(books);
+                    connection.close();
+                }
+            });
+
+
+            request.on('row', (columns) => {
+                let row = {};
+                columns.forEach((column) => {
+                    row[column.metadata.colName] = column.value;
+                });
+                const book: Book = {
+                    book_id: row["book_id"],
+                    ISBN: row["ISBN"],
+                    title: row["title"],
+                    copies_count: row["copies_count"],
+                };
+                books.push(book);
+            });
+
+            request.on('error', (err) => {
+                return res.status(500).json({error: err.message});
+            });
+
+            connection.execSql(request);
         }
         catch(err){
             return res.status(500).json({error: err.message});
